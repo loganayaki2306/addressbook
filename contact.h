175ea391859100c3300237892da730bc1ab755c7
hariprasathkr <ramyariotz22@gmail.com>
2016-03-11 14:19:01 +0530

    fourth commit

---
+++ contact.h
@@ -0,0 +1,44 @@
+#ifndef CORE_CONTACT_H
+#define CORE_CONTACT_H
+
+/************************************************************
+    Class: Contact
+
+    Author: Phil Grohe
+
+    Data structure to hold a single address book contact's
+    info.
+************************************************************/
+
+#include <string>
+#include <vector>
+
+class Contact 
+{
+    public:
+
+        typedef std::vector<Contact> ContactRecordSet;
+        typedef unsigned int ContactId;
+        static const ContactId INVALID_ID = 0;
+
+        ContactId id;
+        std::string firstName;
+        std::string lastName;
+        std::string phoneNumber;
+        std::string address;
+        std::string email;
+
+         std::string rollno;
+          std::string year;
+
+
+        Contact():id(0), firstName(), lastName(), phoneNumber(), email(){ }
+        bool isValidToAdd() const;
+        bool isValidPhoneNo() const;
+        bool isValidEmail() const;
+        bool isEmpty() const;
+
+
+};
+
+#endif
